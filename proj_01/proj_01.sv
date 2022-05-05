efb0081edb2fae66ba803a4124bec035739719ba
Victor Mororo <victormororo@gmail.com>
2022-05-04 23:16:09 -0300

    parte sequencial funcionando, porem ainda falta parte combinatoria

--- proj_01/proj_01.sv
+++ proj_01/proj_01.sv
@@ -1,18 +1,20 @@
-module proj_01 (input logic modo, input clk, reset, output logic [2:0] A, output logic [2:0] B);
+module proj_01 (input logic modo, input clk, reset, output logic [2:0] A, output logic [2:0] B, output logic [2:0] clock_count);
 
 	// 3'b100 -> vermelho
 	// 3'b010 -> amarelo
 	// 3'b001 -> verde
 
-	logic red = 3'b100;
-	logic yel = 3'b010;
-	logic green = 3'b001;
+	reg [2:0] red = 3'b100;
+	reg [2:0] yel = 3'b010;
+	reg [2:0] green = 3'b001;
 
 	reg period;
-	integer clock_count = 0;
+	//reg [2:0] clock_count;    
 
+    reg [2:0] t;
+
+    /*
 	always_comb begin
-
 		if (A == green && B == red)
 			period = 4;	
 		else if (A == yel && B == red)
@@ -25,23 +27,34 @@ module proj_01 (input logic modo, input clk, reset, output logic [2:0] A, output
 			A = green; B = red;
 		end
 	end
+    */
+
+    /*
+	always @(posedge clk, negedge reset) begin
+		if (~reset) begin
+            t <= 0;
+		end else begin
+        end
+    end
+    */
 
 	always @(posedge clk, negedge reset) begin
 		if (~reset) begin
 			A <= green; B <= red;
-			clock_count = 0;
-		end else
-			clock_count += 1;
+			clock_count <= 1;
+		end else begin
+			clock_count <= clock_count + 1;
 			case ({A, B})
-			{green, red} : if (clock_count == period)
-				{A, B} <= {yel, red};
-			{yel, red} : if (clock_count == period)
-				{A, B} <= {red, green};
-			{red, green} : if (clock_count == period)
-				{A, B} <= {red, yel};
-			{red, yel} : if (clock_count == period)
-				{A, B} <= {green, red};
+			{green, red} : if (clock_count == 4) begin
+				{A, B} <= {yel, red}; clock_count <= 1; end
+			{yel, red} : if (clock_count == 1) begin
+				{A, B} <= {red, green};  clock_count <= 1; end
+			{red, green} : if (clock_count == 3) begin
+				{A, B} <= {red, yel};  clock_count <= 1; end
+			{red, yel} : if (clock_count == 1) begin
+				{A, B} <= {green, red};  clock_count <= 1; end
 			endcase
+        end
 		
 	end
 	
